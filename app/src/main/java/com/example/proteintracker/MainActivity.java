50b64505d1eda722dac237b3b358f5b798b485a9
geezann <brayen.chanzezar@si.ukdw.ac.id>
2019-09-04 22:19:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/proteintracker/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.mainActivityTextView);
+        textView.setText(R.string.tes_update_textview);
+    }
+
+
+}
+
